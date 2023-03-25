ca3dc86d480231c95aef5284c98e905919a1fd60
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2023-03-25 11:21:46 +0000

    Update Repository-Stargazers.py

--- Repository-Stargazers.py
+++ Repository-Stargazers.py
@@ -1,1 +1,5 @@
-
+from github import Github
+g_cred = Github("<Insert Your Token Here>")
+f_repo = input("Enter repository name to see its stars (User/Repository): ")
+repo = g_cred.get_repo(f_repo)
+print(repo.stargazers_count)
